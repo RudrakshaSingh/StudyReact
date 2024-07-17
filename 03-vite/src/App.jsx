a57067b25933834739d2ccfe323247d31ecf8740
RudrakshaSingh <rudrakshasingh587@gmail.com>
2024-07-17 08:16:18 -0700

    react elements

--- 03-vite/src/App.jsx
+++ 03-vite/src/App.jsx
@@ -1,9 +1,17 @@
 import Chai from "./chai";
 
+//how to insert variable-using curly braises directly
+//we write an evaluated expression-final outcome of js evaluated
+//we cannot write if else directly on return expression
+//you can do it before /outside return
 function App() {
+    const username = "chai aur code";
+
     return (
         <>
             <Chai />
+            <h1>chai aur react {username}</h1>
+            <p>test para</p>
         </>
     );
 }
--- 03-vite/src/main.jsx
+++ 03-vite/src/main.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import App from "./App.jsx";
+
+function MyApp() {
+    return (
+        <div>
+            <h1>Custom App | chai</h1>
+        </div>
+    );
+}
+
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit google'
+// }
+
+const anotherElement = (
+    <a href="https://google.com" target="_blank">
+        Visit google
+    </a>
+);
+
+const anotherUser = "chai aur react";
+
+//predefined syntax
+const reactElement = React.createElement(
+    "a",
+    { href: "https://google.com", target: "_blank" },
+    "click me to visit google",
+    "br", //it is all becominf single element
+    anotherElement //it can be worked by directly putting it in render or use react.createelement
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(reactElement);
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//     <React.StrictMode>
+//       <App />
+//     </React.StrictMode>,
+//   )
