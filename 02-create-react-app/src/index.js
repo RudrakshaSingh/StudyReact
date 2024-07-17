9d9608a18ccb5cd77f3a8f4f1e799a7ff5433a33
RudrakshaSingh <rudrakshasingh587@gmail.com>
2024-07-17 02:58:47 -0700

    create-react

--- 02-create-react-app/src/index.js
+++ 02-create-react-app/src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//virtual dom- khud ka dom banate hai website wala use nhi karta
+const root = ReactDOM.createRoot(document.getElementById("root"));
+//root ko render karliya //app(jsx) ko render kardiya//can make custom rtag
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//react.strict mode is for development for optimization//it is safe mode ,you can remove it
