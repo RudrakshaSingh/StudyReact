3cef8376b9c1c129eef59a3733230540e16bff9a
RudrakshaSingh <rudrakshasingh587@gmail.com>
2024-07-19 08:40:15 -0700

    props

--- 07-TailwindAndPropsInReact/src/App.jsx
+++ 07-TailwindAndPropsInReact/src/App.jsx
@@ -5,8 +5,7 @@ function App() {
     return (
         <>
             <h1 className="bg-green-400 mb-4 text-black p-4 rounded-xl">Tailwind Test</h1>
-
-            <Card />
+            <Card character="rudra" btnText="Visit me sexy" />
         </>
     );
 }
--- 07-TailwindAndPropsInReact/src/components/Card.jsx
+++ 07-TailwindAndPropsInReact/src/components/Card.jsx
@@ -1,4 +1,4 @@
-function Card() {
+function Card({ character, btnText }) {
     return (
         <div className="relative h-[400px] w-[300px] rounded-md">
             <img
@@ -8,12 +8,12 @@ function Card() {
             />
             <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
             <div className="absolute bottom-4 left-4 text-left">
-                <h1 className="text-lg font-semibold text-white">Delba</h1>
+                <h1 className="text-lg font-semibold text-white">{character}</h1>
                 <p className="mt-2 text-sm text-gray-300">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, debitis?
                 </p>
                 <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white bg-black">
-                    View Profile →
+                    {btnText} →
                 </button>
             </div>
         </div>
